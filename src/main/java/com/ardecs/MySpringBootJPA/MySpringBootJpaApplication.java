9569ab36e33d3b671a5327e43b234a80e8b72ee9
kentyku <kentyku@box>
2018-11-05 22:50:05 +0400

    Начало разработки

---
+++ src/main/java/com/ardecs/MySpringBootJPA/MySpringBootJpaApplication.java
@@ -0,0 +1,12 @@
+package com.ardecs.MySpringBootJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootJpaApplication.class, args);
+	}
+}
