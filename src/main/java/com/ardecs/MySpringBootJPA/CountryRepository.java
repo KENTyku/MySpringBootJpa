1927dbbead65480e58dc5ac4b51eba8da1f78463
Yuri Tveritin <kentyku@bk.ru>
2018-11-08 13:26:23 +0400

    добавил свои методы в CountryRepository

--- src/main/java/com/ardecs/MySpringBootJPA/CountryRepository.java
+++ src/main/java/com/ardecs/MySpringBootJPA/CountryRepository.java
@@ -12,6 +12,9 @@ import org.springframework.data.repository.CrudRepository;
  */
 
 public interface CountryRepository extends CrudRepository<Country, Long> {
+    Country findByCountryName(String name);
+    Country findById(int id);
+
 
 
 }
--- src/test/java/com/ardecs/MySpringBootJPA/MySpringBootJpaApplicationTests.java
+++ src/test/java/com/ardecs/MySpringBootJPA/MySpringBootJpaApplicationTests.java
@@ -1,46 +1,36 @@
 package com.ardecs.MySpringBootJPA;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceContext;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/Config.xml"})
 public class MySpringBootJpaApplicationTests {
     @Autowired
     private CountryRepository countryRepository;
-    @PersistenceContext()
-    EntityManager em;
 
     @Test
-    public void contextLoads() {
+    public void testCrud() {
 
-
-//        JpaTransactionManager txnManager = null;
-//        TransactionStatus status =
-//                txnManager.getTransaction(new DefaultTransactionDefinition());
         ArrayList<Country> countries = (ArrayList<Country>) countryRepository.findAll();
         for (Country country : countries) {
             System.out.println(country.toString());
         }
-//        txnManager.commit(status);
-
-        Country country2=new Country("USA");
+        Country country4 = new Country("123");
+        countryRepository.save(country4);
+        Country country2=countryRepository.findById(72);
+        country2.setCountryName("Test");
         countryRepository.save(country2);
+//        countryRepository.delete(country4);
+        System.out.println("TEST "+country2.toString());
+
+//
     }
 
 }
