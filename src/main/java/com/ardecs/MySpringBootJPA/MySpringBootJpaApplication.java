7638dbf401babd608dc1ba0d19e2bbae0ae28990
Yuri Tveritin <kentyku@bk.ru>
2018-11-12 09:12:27 +0400

    work

--- src/main/java/com/ardecs/MySpringBootJPA/MySpringBootJpaApplication.java
+++ src/main/java/com/ardecs/MySpringBootJPA/MySpringBootJpaApplication.java
@@ -3,10 +3,34 @@ package com.ardecs.MySpringBootJPA;
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.ArrayList;
+//@Controller
 //@SpringBootApplication
+//@ContextConfiguration(locations = {"classpath:META-INF/Config.xml"})//указываем конфиг для работы автокофигурации контекста в тесте
 public class MySpringBootJpaApplication {
+//    @Autowired
+//    static private CountryRepository countryRepository;
 
     public static void main(String[] args) {
 //        SpringApplication.run(MySpringBootJpaApplication.class, args);
-            }
+//
+//        ArrayList<Country> countries = (ArrayList<Country>) countryRepository.findAll();
+//        for (Country country : countries) {
+//            System.out.println(country.toString());
+//        }
+//        Country country1 = new Country("123");
+//        countryRepository.save(country1);
+//        Country country2 = countryRepository.findById(Long.valueOf(76)).get();
+//        country2.setCountryName("Test");
+//        countryRepository.save(country2);
+//        countryRepository.delete(country1);
+//        System.out.println("TEST " + country2.toString());
+////        countryRepository.deleteById(Long.valueOf(75));
+//
+
+    }
 }
